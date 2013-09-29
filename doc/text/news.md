bf16e80a9dd0d5c1f0db4ee2dbdbb1056dc2e1ad
Kouhei Sutou <kou@clear-code.com>
2013-09-29 18:04:25 +0900

    Extract from groonga-command gem
    Because parser needs YAJL. If a library needs only
    Groonga::Command::*, YAJL dependency is harmful because it doesn't
    provide x64-mingw32 binary for now.

---
+++ doc/text/news.md
@@ -0,0 +1,5 @@
+# News
+
+## 1.0.0: 2013-09-29
+
+The first release!!! It is extracted from groonga-command gem.
---
+++ lib/groonga/command/parser/error.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2013  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "groonga/command"
+
+module Groonga
+  module Command
+    class Parser
+      class Error < Command::Error
+        attr_reader :reason, :location
+        def initialize(reason, before, after)
+          @reason = reason
+          @location = compute_location(before, after)
+          super("#{@reason}:\n#{@location}")
+        end
+
+        private
+        def compute_location(before, after)
+          location = ""
+          if before[-1] == ?\n
+            location << before
+            location << after
+            location << "^"
+          elsif after[0] == ?\n
+            location << before
+            location << "\n"
+            location << " " * before.bytesize + "^"
+            location << after
+          else
+            location << before
+            location << after
+            location << " " * before.bytesize + "^"
+          end
+          location
+        end
+      end
+    end
+  end
+end
