3259b7affc5812b87525664405f5a8a445e7045e
Kouhei Sutou <kou@clear-code.com>
2015-10-14 17:37:59 +0900

    FORCE_FFI_YAJL is needed

--- lib/groonga/command/parser/load-values-parser.rb
+++ lib/groonga/command/parser/load-values-parser.rb
@@ -16,6 +16,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+ENV["FORCE_FFI_YAJL"] = "ffi"
 require "ffi_yajl"
 
 module FFI_Yajl
